b5be069c9d7cf281b456b5f95ae63152851add8d
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-03-27 17:54:24 +0200

    add doctors table to migration

--- database/migrations/2023_03_27_154819_create_doctors_table.php
+++ database/migrations/2023_03_27_154819_create_doctors_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->foreign('pharmacy_id')->references('id')->on('pharmacies');
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_id");
+            $table->unsignedBigInteger("pharmacy_id");
+            $table->timestamps();
         });
     }
 
@@ -21,8 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('doctors');
     }
 };
