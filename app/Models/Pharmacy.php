2334bb0dd5dd0362a94621454a93168f94a7a5ac
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-03-27 23:13:41 +0200

    add pharmacy model relations

---
+++ app/Models/Pharmacy.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pharmacy extends Model
+{
+    use HasFactory;
+
+
+    public function doctors()
+    {
+        return $this->hasMany(Doctor::class,'pharmacy_id');
+    }
+
+    public function area()
+    {
+        return $this->belongsTo(Area::class,'area_id');
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class,'pharmacy_id');
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class,'owner_id');
+    }
+
+
+}
