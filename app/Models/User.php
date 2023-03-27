56602a376729908363a30fa3f63196a7eda0fe66
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-03-27 23:54:46 +0200

    add user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,22 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function addresses()
+    {
+        return $this->hasMany(Address::class,"user_id");
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class,"user_id");
+    }
+
+
+    public function pharmacy()
+    {
+        return $this->hasOne(Pharmacy::class,"owner_id");
+    }
+
 }
