fcd5c51e5fd21f6f718934d5b19c3d308627d362
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-03-27 22:39:08 +0200

    add laravel ui

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name("index");
+
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
