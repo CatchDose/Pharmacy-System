464fe3c4ec3139456a855f92c8621f3003612ee1
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-03-27 23:48:06 +0200

    add order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -15,4 +15,20 @@ class Order extends Model
         return $this->belongsToMany(Medicine::class,'medicines_orders'
             ,'order_id' ,'medicine_id');
     }
+
+    public function pharmacy()
+    {
+        return $this->belongsTo(Pharmacy::class,"pharmacy_id");
+    }
+
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class,"doctor_id",'user_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,"user_id");
+    }
 }
