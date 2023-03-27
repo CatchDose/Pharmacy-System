c167671d9a0456eb7214ba981796c17b263fdbfc
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-03-27 22:29:02 +0200

    add type to medicines

--- database/migrations/2023_03_27_150031_create_medicines_table.php
+++ database/migrations/2023_03_27_150031_create_medicines_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('medicines', function (Blueprint $table) {
             $table->id();
             $table->string("name");
+            $table->string("type");
             $table->integer("price");
 
             $table->timestamps();
