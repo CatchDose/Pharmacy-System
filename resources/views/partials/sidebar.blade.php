5cd72db57da2ccf77f259b1cfc3dec5a93e854f2
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-03-28 22:29:02 +0200

    add sidebar icons

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -34,11 +34,22 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
+
+                {{--Home Page--}}
+                <li class="nav-item">
+                    <a href="{{route("index")}}" class="nav-link">
+                        <i class="fas fa-home nav-icon"></i>
+                        <p>Home</p>
+                    </a>
+                </li>
+
+
                 <li class="nav-item menu-open">
+
                     <a href="#" class="nav-link active">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
-                            Starter Pages
+                            users
                             <i class="right fas fa-angle-left"></i>
                         </p>
                     </a>
@@ -46,28 +57,17 @@
                         <li class="nav-item">
                             <a href="#" class="nav-link active">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Active Page</p>
+                                <p>all users</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="#" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Inactive Page</p>
+                                <p>add user</p>
                             </a>
                         </li>
 
-                        <li class="nav-item">
-                            <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
 
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>logout</p>
-                            </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-
-                        </li>
 
                     </ul>
                 </li>
@@ -79,6 +79,19 @@
                             <span class="right badge badge-danger">New</span>
                         </p>
                     </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>logout</p>
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+
                 </li>
             </ul>
         </nav>
