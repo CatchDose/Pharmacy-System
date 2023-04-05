d0fd1361b49a41795f4aac20077d3ce1731389c3
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-04-06 01:37:49 +0200

    refactor sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -290,15 +290,6 @@
                 </li>
                 @endhasanyrole
 
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-th"></i>
-                        <p>
-                            Simple Link
-                            <span class="right badge badge-danger">New</span>
-                        </p>
-                    </a>
-                </li>
 
                 <li class="nav-item">
                     <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault();
