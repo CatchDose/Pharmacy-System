a9cabb08f481893316363bf86304ebb4a8d9fc98
mahmoud elbasiony <mahmoud.elbasiony904@gmail.com>
2023-04-06 16:36:47 +0200

    feat-chart

--- resources/views/medicines/index.blade.php
+++ resources/views/medicines/index.blade.php
@@ -44,14 +44,39 @@
 
 @section('scripts')
 {{ $dataTable->scripts() }}
-   <script>
-    function modalShow(event){
+
+    <script>
+        var toast=new bootstrap.Toast(document.querySelector(".toast"),{"delay":5000});
+
+        function modalShow(event){
         event.preventDefault();
         event.stopPropagation();
+
         document.querySelector(".modal-footer").lastElementChild.addEventListener("click",(e)=>{
-            event.target.closest("form").submit();
-        })
+
+        fetch(event.target.closest("form").action,{
+        method: "DELETE",
+        headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}
+    })
+        .then((res,err)=>{
+        document.getElementById("modalNo").click();
+        return res.json();
+    })
+        .then(res=>{
+        console.log(res);
+        if(res["success"]){
+        document.querySelector(".toast-body").innerHTML=`<i class="bi bi-check-circle text-success fs-3 me-2"></i> ${res["success"]}`;
+        toast.show()
+        $('#medicines-table').DataTable().ajax.reload();
+    }else{
+        document.querySelector(".toast-body").innerHTML=`<i class="bi bi-exclamation-circle text-danger fs-3 me-2"></i> ${res["error"]}`;
+        toast.show()
     }
+    })
+    })
+    }
+
+
    </script>
 
 @endsection
