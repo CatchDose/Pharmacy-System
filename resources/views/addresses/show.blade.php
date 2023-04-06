c6857b90ba602ff9c6e5d989a5f13a95b25df963
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-04-06 17:25:07 +0200

    delete the title

--- resources/views/addresses/show.blade.php
+++ resources/views/addresses/show.blade.php
@@ -18,10 +18,6 @@
 @section("content")
 
     <div class="card card-primary">
-        <div class="card-header">
-            <h3 class="card-title"> Address Info </h3>
-        </div>
-
         <div class="card-body">
             <strong> User Name</strong>
             <p class="text-muted">
